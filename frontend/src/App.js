0166a6955a62ab24d190c6a14db404385c50222c
manishsimplegithub <manishsrivastav@bbdu.ac.in>
2022-04-21 14:40:49 +0530

    created routes

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/main/login";
+import Admin from "./components/admin";
+import User from "./components/user";
+import Main from "./components/main";
+import Dashboard from "./components/admin/dashboard";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Admin />} path="admin">
+          <Route element={<Dashboard />} path="dashboard" />
+        </Route>
+        <Route element={<User />} path="user"></Route>
+        <Route element={<Main />} path="main"></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
